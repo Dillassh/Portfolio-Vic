da3767cd6099b9f83d4aa2860c3bca840fa8f1f3
Dillassh <dill.assh@gmail.com>
2022-10-26 23:15:54 +0300

    modifications

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,19 +1,84 @@
 import React from 'react'
 import "./Portfolio.css"
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title:'Refresh Your House',
+    github: "https://github.com/Dillassh/refreshouse",
+    demo: "https://refreshouse.netlify.com"
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title:'Dillash Store',
+    github: "https://github.com/Dillassh/Dillash-store",
+    demo: "http://dillash-store.netlify.com"
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title:'Blog',
+    github: "https://github.com/Dillassh/Blog_react",
+    demo: "https://fancy-cucurucho-664ecd.netlify.app"
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title:'E-commerce',
+    github: "https://github.com/Dillassh/emag-try",
+    demo: "https://refreshouse.netlify.com"
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title:'Portfolio',
+    github: "https://github.com/Dillassh/Portfolio-Vic",
+    demo: "https://working-hard.netlify.app"
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title:'Refresh Your House',
+    github: "https://github.com/Dillassh/refreshouse",
+    demo: "https://working-hard.netlify.app"
+  }
+]
 
 const Portfolio = () => {
+
+
+
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
         
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image"></div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn' >Github</a>
-          <a href="https://github.com" className='btn btn-primary' >Live Demo</a>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+            <article key={id} className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={image} alt={title} />
+          </div>
+          <h3>{title}</h3>
+          <div className="portfolio__item-cta"> 
+           <a href={github} className='btn' >Github</a>
+           <a href={demo} className='btn btn-primary' >Live Demo</a></div>
         </article>
+        )
+          })
+        }
+        
         </div> 
     </section>
   )
